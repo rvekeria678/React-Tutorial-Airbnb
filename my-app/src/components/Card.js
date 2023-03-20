ab73926f206895b46bda4f47beb830c5280f6ef6
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2023-03-20 12:19:35 -0400

    Hard Coded Prop Component

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -1,18 +1,20 @@
 import React from "react";
+import Star from "../images/star.PNG";
+import Photo from "../images/Hero1.png";
 import "../css/Card.css";
 
 export default function Card () {
     return (
         <div className="card">
-            <img src="" alt="Resource Not Found" className="card--image"></img>
+            <img src={Photo} alt="Resource Not Found" className="card--image"></img>
             <div className="card--stats">
-                <img src=""></img>
+                <img className="star" src={Star} alt="Resource not Found."></img>
                 <span>5.0</span>
-                <span>(6)</span>
-                <span>USA</span>
+                <span className="gray">(6)</span>
+                <span className="gray">USA</span>
             </div>
             <p className="card--header">Life Lessons with Katie Zaferes</p>
-            <p className=""><span className="">From $136</span>/ person</p>
+            <p className=""><span className="bold">From $136</span>/ person</p>
         </div>
     )
 }
