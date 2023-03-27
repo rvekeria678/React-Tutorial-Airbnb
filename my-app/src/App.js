03cc2a24438f721f018e9380e1c72f60d743fd3c
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2023-03-27 15:31:47 -0400

    Added props component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import Hero1 from "./images/Hero1.png";
+import Hero2 from "./images/Hero2.jpg";
+import Hero3 from "./images/Hero3.jpg";
 
 function App() {
   return (
@@ -9,10 +12,30 @@ function App() {
       <Navbar />
       <Hero />
       <div class="card--panel">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card 
+          img={Hero1}
+          rating="5.0"
+          reviewCount={6}
+          country="USA"
+          title="Life Lessons with Katie Zaferes"
+          price={136}
+        />
+        <Card 
+          img={Hero2}
+          rating="5.0"
+          reviewCount={30}
+          country="USA"
+          title="Learn wedding photography"
+          price={125}
+        />
+        <Card
+          img={Hero3}
+          rating="4.8"
+          reviewCount={2}
+          country="USA"
+          title="Group Mountain Biking"
+          price={50}
+        />
       </div>
     </div>
   );
--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -2,17 +2,18 @@ import React from "react";
 import "../css/Card.css";
 
 export default function Card (props) {
-    console.log(props)
+    console.log("IMAGE PATH -> ",props.img);
+    const img_link = props.img;
     return (
         <div className="card">
-            <img src={require("../images/Hero1.png")} alt="Resource Not Found!" className="card--image"></img>
+            <img src={img_link} alt="Resource Not Found!" className="card--image"></img>
             <div className="card--stats">
                 <img className="star" src={require("../images/star.PNG")} alt="Resource not Found."></img>
-                <span>5.0</span>
-                <span className="gray">(6)</span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount})</span>
+                <span claddddddddssName="gray">{props.country}</span>
             </div>
-            <p className="card--header">Life Lessons with Katie Zaferes</p>
+            <p className="card--header">{props.title}</p>
             <p className=""><span className="bold">From ${props.price}</span>/ person</p>
         </div>
     )
